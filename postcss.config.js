02837ab8520e973796dc27d144667b83b240ab21
chandan-16 <chandangautam420.cg@gmail.com>
2025-07-03 11:07:55 +0530

    Engineer'S Cradle Assignment

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+import tailwind from '@tailwindcss/postcss';
+import autoprefixer from 'autoprefixer';
+
+/** @type {import('postcss').ProcessOptions} */
+export default {
+  plugins: [
+    tailwind(),
+    autoprefixer(),
+  ],
+};
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="bg-white text-red-500 p-4 text-center">
+      Tailwind is working! 🎉
+    </div>
+    </>
+  )
+}
+
+export default App
